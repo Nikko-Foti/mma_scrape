59da7fb5d9f8a4e2d855f4fceae085d843f667b9
Nikko Foti <nicholas.foti@rakuten.com>
2017-11-11 13:00:40 -0800

    edited config file

--- src/config.js
+++ src/config.js
@@ -1,42 +1,42 @@
 const queries = {
-    insertFlyweightQuery: 'INSERT INTO weight_classes.flyweight' +
+    insertFlyweightQuery: 'INSERT INTO flyweight' +
         ' (first_name, last_name, nickname, association, hometown, nationality, age, height, college, degree, style, weight_class, rank, wins, losses, draws, strikes, takedowns, thumbnail, belt_thumbnail, left_full_body_image, right_full_body_image, profile_image, fights)' + 
         ' VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-    insertBantamweightQuery: 'INSERT INTO weight_classes.bantamweight' +
+    insertBantamweightQuery: 'INSERT INTO bantamweight' +
         ' (first_name, last_name, nickname, association, hometown, nationality, age, height, college, degree, style, weight_class, rank, wins, losses, draws, strikes, takedowns, thumbnail, belt_thumbnail, left_full_body_image, right_full_body_image, profile_image, fights)' + 
         ' VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-    insertFeatherweightQuery: 'INSERT INTO weight_classes.featherweight' +
+    insertFeatherweightQuery: 'INSERT INTO featherweight' +
         ' (first_name, last_name, nickname, association, hometown, nationality, age, height, college, degree, style, weight_class, rank, wins, losses, draws, strikes, takedowns, thumbnail, belt_thumbnail, left_full_body_image, right_full_body_image, profile_image, fights)' + 
         ' VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-    insertLightweightQuery: 'INSERT INTO weight_classes.lightweight' +
+    insertLightweightQuery: 'INSERT INTO lightweight' +
         ' (first_name, last_name, nickname, association, hometown, nationality, age, height, college, degree, style, weight_class, rank, wins, losses, draws, strikes, takedowns, thumbnail, belt_thumbnail, left_full_body_image, right_full_body_image, profile_image, fights)' + 
         ' VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-    insertWelterweightQuery: 'INSERT INTO weight_classes.welterweight' +
+    insertWelterweightQuery: 'INSERT INTO welterweight' +
         ' (first_name, last_name, nickname, association, hometown, nationality, age, height, college, degree, style, weight_class, rank, wins, losses, draws, strikes, takedowns, thumbnail, belt_thumbnail, left_full_body_image, right_full_body_image, profile_image, fights)' + 
         ' VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-    insertMiddleweightQuery: 'INSERT INTO weight_classes.middleweight' +
+    insertMiddleweightQuery: 'INSERT INTO middleweight' +
         ' (first_name, last_name, nickname, association, hometown, nationality, age, height, college, degree, style, weight_class, rank, wins, losses, draws, strikes, takedowns, thumbnail, belt_thumbnail, left_full_body_image, right_full_body_image, profile_image, fights)' + 
         ' VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-    insertLight_HeavyweightQuery: 'INSERT INTO weight_classes.light_heavyweight' +
+    insertLight_HeavyweightQuery: 'INSERT INTO light_heavyweight' +
         ' (first_name, last_name, nickname, association, hometown, nationality, age, height, college, degree, style, weight_class, rank, wins, losses, draws, strikes, takedowns, thumbnail, belt_thumbnail, left_full_body_image, right_full_body_image, profile_image, fights)' + 
         ' VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-    insertHeavyweightQuery: 'INSERT INTO weight_classes.heavyweight' +
+    insertHeavyweightQuery: 'INSERT INTO heavyweight' +
         ' (first_name, last_name, nickname, association, hometown, nationality, age, height, college, degree, style, weight_class, rank, wins, losses, draws, strikes, takedowns, thumbnail, belt_thumbnail, left_full_body_image, right_full_body_image, profile_image, fights)' + 
         ' VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-    insertWomen_StrawweightQuery: 'INSERT INTO weight_classes.Women_Strawweight' +
+    insertWomen_StrawweightQuery: 'INSERT INTO Women_Strawweight' +
         ' (first_name, last_name, nickname, association, hometown, nationality, age, height, college, degree, style, weight_class, rank, wins, losses, draws, strikes, takedowns, thumbnail, belt_thumbnail, left_full_body_image, right_full_body_image, profile_image, fights)' + 
         ' VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-    insertWomen_BantamweightQuery: 'INSERT INTO weight_classes.Women_Bantamweight' +
+    insertWomen_BantamweightQuery: 'INSERT INTO Women_Bantamweight' +
         ' (first_name, last_name, nickname, association, hometown, nationality, age, height, college, degree, style, weight_class, rank, wins, losses, draws, strikes, takedowns, thumbnail, belt_thumbnail, left_full_body_image, right_full_body_image, profile_image, fights)' + 
         ' VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-    insertWomen_FeatherweightQuery: 'INSERT INTO weight_classes.Women_Featherweight' +
+    insertWomen_FeatherweightQuery: 'INSERT INTO Women_Featherweight' +
         ' (first_name, last_name, nickname, association, hometown, nationality, age, height, college, degree, style, weight_class, rank, wins, losses, draws, strikes, takedowns, thumbnail, belt_thumbnail, left_full_body_image, right_full_body_image, profile_image, fights)' + 
         ' VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
 }
 
 var dbConstants = {
     endpoint : '192.168.2.66:9042',
-    keyspace : 'weight_classes_two', 
+    keyspace : 'classes', 
 }
 
 module.exports = {
